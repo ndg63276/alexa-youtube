dff8bc7cdfec6b5024d43bd1a8b416ce5a72c161
Mark Williams <markwilliams4@gmail.com>
2019-06-22 20:37:04 +0100

    Use config file

--- test_functions/invoke_lambda_function.sh
+++ test_functions/invoke_lambda_function.sh
@@ -2,18 +2,32 @@
 
 gitroot=`git rev-parse --show-toplevel`
 
-regions="us-east-1
-eu-west-1
-us-west-2
-ap-northeast-1
-"
+. config.cfg
+
+function invoke {
+  intent=$1
+  local -n arr=$2
+  if [[ ${arr[type]} == 'live' ]]; then
+    region=${arr[region]}
+    arn_no=${arr[arn]}
+    name=${arr[name]}
+    arn=arn:aws:lambda:$region:$arn_no:function:$name
+    echo $arn $intent $region
+    aws lambda --region $region invoke --function-name $arn --payload fileb://$gitroot/test_intents/$intent /dev/null | grep StatusCode
+  fi
+}
 
 intents=`ls $gitroot/test_intents/`
 
 for intent in $intents; do
-  for region in $regions; do
-    arn="arn:aws:lambda:$region:175548706300:function:YouTube"
-    echo $arn $intent $region
-    aws lambda --region $region invoke --function-name $arn --payload fileb://$gitroot/test_intents/$intent /dev/null | grep StatusCode
-  done
+  invoke $intent config1
+  invoke $intent config2
+  invoke $intent config3
+  invoke $intent config4
+  invoke $intent config5
+  invoke $intent config6
+  invoke $intent config7
+  invoke $intent config8
+  invoke $intent config9
+  invoke $intent config10
 done
