1b63f7998f5f1431341e595a5430d3263f07751b
Mark Williams <mark.williams@diamond.ac.uk>
2019-06-28 10:08:40 +0100

    bash 4.2 compliant versions

--- test_functions/upload_lambda.sh
+++ test_functions/upload_lambda.sh
@@ -5,7 +5,7 @@ gitroot=`git rev-parse --show-toplevel`
 . config.cfg
 
 function upload {
-  local -n arr=$1
+  eval "declare -A arr="${1#*=}
   if [[ ${arr[type]} == 'live' ]]; then
     region=${arr[region]}
     arn_no=${arr[arn]}
@@ -15,13 +15,14 @@ function upload {
   fi
 }
 
-upload config1
-upload config2
-upload config3
-upload config4
-upload config5
-upload config6
-upload config7
-upload config8
-upload config9
-upload config10
+if [[ ${config1[type]} != "" ]]; then upload "$(declare -p config1)"; fi
+if [[ ${config2[type]} != "" ]]; then upload "$(declare -p config2)"; fi
+if [[ ${config3[type]} != "" ]]; then upload "$(declare -p config3)"; fi
+if [[ ${config4[type]} != "" ]]; then upload "$(declare -p config4)"; fi
+if [[ ${config5[type]} != "" ]]; then upload "$(declare -p config5)"; fi
+if [[ ${config6[type]} != "" ]]; then upload "$(declare -p config6)"; fi
+if [[ ${config7[type]} != "" ]]; then upload "$(declare -p config7)"; fi
+if [[ ${config8[type]} != "" ]]; then upload "$(declare -p config8)"; fi
+if [[ ${config9[type]} != "" ]]; then upload "$(declare -p config9)"; fi
+if [[ ${config10[type]} != "" ]]; then upload "$(declare -p config10)"; fi
+
