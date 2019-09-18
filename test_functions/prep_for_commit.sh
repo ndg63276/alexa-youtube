797b6a6e7f279b3a144f4c188460ae0a56d555c1
Mark Williams <mark.williams@diamond.ac.uk>
2019-09-18 13:33:28 +0100

    Create a prep for commit script, with auto update date #109

---
+++ test_functions/prep_for_commit.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+gitroot=`git rev-parse --show-toplevel`
+rm $gitroot/lambda_function.zip 2>/dev/null
+today=`date +"%d %b %Y"`
+sed -i "/Last update/c\__Last update: ${today}__" $gitroot/README.md
+
+
+GLOBIGNORE="*"
+command="cd $gitroot; zip -r lambda_function.zip * -x \".git*\" -x \"test_functions*\" -x \"test_intents*\""
+for line in `cat $gitroot/.gitignore`; do
+    command+=" -x \"$line\""
+done
+echo $command
+#$command 
