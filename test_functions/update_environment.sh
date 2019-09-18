8e9c1680e635274110e56a9916a3a5c8cea9eb85
Mark Williams <mark.williams@diamond.ac.uk>
2019-09-18 16:49:05 +0100

    Use list functions script

--- test_functions/update_environment.sh
+++ test_functions/update_environment.sh
@@ -1,28 +1,22 @@
 #!/bin/bash
 
-new_key=$1
-new_val=$2
+tag=$1
+new_key=$2
+new_val=$3
+
+gitroot=`git rev-parse --show-toplevel`
 
 function update {
-	region=$1
-	arn=$2
+	arn=$1
+	region=`echo $arn | cut -d: -f4`
 	function_config=`aws lambda --region $region get-function-configuration --function-name $arn`
 	old_environment=`echo "$function_config" | jq  ".Environment"`
 	new_environment=`echo "$old_environment" | jq ".Variables.$new_key = \"$new_val\""`
 	aws lambda --region $region update-function-configuration --function-name $arn --environment "$new_environment"
 }
 
-
-regions="ap-northeast-1 eu-west-1 us-east-1 us-west-2"
-for region in $regions; do
-	function_list=`aws lambda --region $region list-functions`
-	arn_list=`echo $function_list | jq -r ".Functions[].FunctionArn"`
-
-	for arn in $arn_list; do
-		tag_list=`aws lambda --region $region list-tags --resource $arn`
-		if jq -e '.Tags | has("Patreon")' >/dev/null <<< $tag_list; then
-			update $region $arn
-		fi
-	done
+arns=`$gitroot/test_functions/list_lambda_functions.sh $tag`
+for arn in $arns; do
+	update $arn
 done
 
