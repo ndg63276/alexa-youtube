e0afa7f9b4abfcd755e8bfc7c5829405bf734fc0
Mark Williams <mark.williams@diamond.ac.uk>
2019-09-18 16:47:38 +0100

    List functions should be its own script

---
+++ test_functions/list_lambda_functions.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+tag=$1
+
+regions="ap-northeast-1 eu-west-1 us-east-1 us-west-2"
+for region in $regions; do
+	function_list=`aws lambda --region $region list-functions`
+	arn_list=`echo $function_list | jq -r ".Functions[].FunctionArn"`
+
+	for arn in $arn_list; do
+		tag_list=`aws lambda --region $region list-tags --resource $arn`
+		if jq -e ".Tags | has(\"$tag\")" >/dev/null <<< $tag_list; then
+			echo $arn
+		fi
+	done
+done
+
