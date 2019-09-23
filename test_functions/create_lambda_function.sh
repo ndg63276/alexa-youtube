6b936d302a3037523e7f47a5599530796820e402
Mark Williams <markwilliams4@gmail.com>
2019-09-23 22:40:45 +0100

    Add new tags when creating functions

--- test_functions/create_lambda_function.sh
+++ test_functions/create_lambda_function.sh
@@ -15,8 +15,9 @@ fi
 DEVELOPER_KEY=`$gitroot/test_functions/get_developer_key.sh`
 ROLE="arn:aws:iam::175548706300:role/lambda_basic_execution"
 
-environment="{ \"Variables\":{\"DEVELOPER_KEY\":\"$DEVELOPER_KEY\",\"PatreonAmount\":\"$patreonamount\",\"Patreon\":\"$patreon\" } }"
+environment="{ \"Variables\":{\"DEVELOPER_KEY\":\"$DEVELOPER_KEY\" } }"
+tags="{\"PatreonAmount\":\"$patreonamount\",\"Patreon\":\"$patreon\" }"
 
-aws lambda --region $region create-function --function-name $name --runtime python2.7 --role $ROLE --handler lambda_function.lambda_handler --zip-file fileb://$gitroot/lambda_function.zip --timeout 10 --memory-size 512 --environment "$environment"
+aws lambda --region $region create-function --function-name $name --runtime python2.7 --role $ROLE --handler lambda_function.lambda_handler --zip-file fileb://$gitroot/lambda_function.zip --timeout 10 --memory-size 512 --environment "$environment" --tags "$tags"
 
 aws lambda --region $region add-permission --function-name $name --statement-id 1 --action "lambda:InvokeFunction" --principal "alexa-appkit.amazon.com"
