075e758a6d630eea6aefab5efe78ed43cc2fa39c
Mark Williams <markwilliams4@gmail.com>
2019-10-14 20:51:22 +0100

    Set environment vars when creating functions

--- test_functions/get_environment_variables.sh
+++ test_functions/get_environment_variables.sh
@@ -4,6 +4,6 @@ arn="arn:aws:lambda:eu-west-1:175548706300:function:YouTubeTest"
 region=`echo $arn | cut -d: -f4`
 
 function_config=`aws lambda --region $region get-function-configuration --function-name $arn`
-dev_key=`echo "$function_config" | jq -r ".Environment.Variables.DEVELOPER_KEY"`
+env_vars=`echo "$function_config" | jq -r ".Environment.Variables"`
 
-echo $dev_key
+echo $env_vars
