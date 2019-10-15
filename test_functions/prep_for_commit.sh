f1bd976f7e88385ece99238935d605d18ac42782
Mark Williams <markwilliams4@gmail.com>
2019-10-15 21:27:50 +0100

    Dont use .gitignore as a zipignore

--- test_functions/prep_for_commit.sh
+++ test_functions/prep_for_commit.sh
@@ -2,14 +2,11 @@
 
 gitroot=`git rev-parse --show-toplevel`
 rm $gitroot/lambda_function.zip 2>/dev/null
+rm *~ 2>/dev/null
 today=`date +"%d %b %Y"`
 sed -i "/Last update/c\__Last update: ${today}__" $gitroot/README.md
 
 
 GLOBIGNORE="*"
 command="cd $gitroot; zip -r lambda_function.zip * -x \".git*\" -x \"test_functions*\" -x \"test_intents*\" -x \"InteractionModel*\""
-for line in `cat $gitroot/.gitignore`; do
-    command+=" -x \"$line\""
-done
 echo $command
-#$command 
