532284e58fb0128adaf25eb387ab72b0e0bda24b
Mark Williams <24956497+ndg63276@users.noreply.github.com>
2019-11-07 20:42:27 +0000

    Create functions in python3.7

--- test_functions/create_lambda_function.sh
+++ test_functions/create_lambda_function.sh
@@ -23,6 +23,6 @@ ROLE="arn:aws:iam::175548706300:role/lambda_basic_execution"
 environment="{ \"Variables\": $ENVIRONMENT_VARIABLES }"
 tags="{\"PatreonAmount\":\"$patreonamount\",\"Patreon\":\"$patreon\" }"
 
-aws lambda --region $region create-function --function-name $name --runtime python2.7 --role $ROLE --handler lambda_function.lambda_handler --zip-file fileb://$gitroot/lambda_function.zip --timeout 10 --memory-size 512 --environment "$environment" --tags "$tags"
+aws lambda --region $region create-function --function-name $name --runtime python3.7 --role $ROLE --handler lambda_function.lambda_handler --zip-file fileb://$gitroot/lambda_function.zip --timeout 10 --memory-size 512 --environment "$environment" --tags "$tags"
 
 aws lambda --region $region add-permission --function-name $name --statement-id 1 --action "lambda:InvokeFunction" --principal "alexa-appkit.amazon.com"
